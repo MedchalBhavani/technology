d5b3ee39b6f97b3221441d0068e10f9690a5e232
MedchalBhavani <162301343+MedchalBhavani@users.noreply.github.com>
2024-04-05 16:36:12 +0530

    added js file

---
+++ airline.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function checkInput() {
+    const email = prompt(`Enter mail ID `);
+    const password = prompt(`Enter password`)
+    if (email =="abc" && password=="123") {
+        alert("Success!");
+    } else {
+        alert("Error");
+    }
+}
+
+/*
+function checkInput(buttonId) {
+    const input = prompt(`Enter mail ID ${buttonId}:`);
+    const targetValue = (buttonId === "button1") ? "abc" : "123";
+    if (input === targetValue) {
+        alert("Success!");
+    } else {
+        alert("Error");
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+ /*function ToExecute(log,pass)
+{
+
+
+    const log = document.getElementsByClassName("Login").value;
+const pass = document.getElementsByClassName("Password").value;
+    if(log == "abc" && pass == "123"){
+        alert("success");
+    }
+    else{
+        alert("error");
+    }
+
+
+
+   
+}
+*/
+
+
+
+
+   
